e09682e931828a12f88eda5fc01c5e3de7e3be54
Vishal-Borase <vishalborse2885@gmail.com>
2024-12-30 18:02:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyrohelpo",
-    version="4.1.4",
+    version="4.1.5",
     author="Vishal-1756",
     author_email="vishalborse199@gmail.com",
     description="A powerful pagination library for Pyrogram bots",
@@ -27,7 +27,7 @@ setup(
     ],
     python_requires=">=3.7",
     install_requires=[
-        "pyrogram",
+        "kurigram",
         "tgcrypto",
     ],
     keywords=["pyrogram", "telegram", "bot", "pagination", "help-menu"],
